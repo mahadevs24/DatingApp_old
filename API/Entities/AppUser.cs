5b6d08f79bea4b9c199d9bd89f97ef260d07b861
mahadevs24 <mahadevshirodkar2@gmail.com>
2023-01-07 18:34:00 +0530

    second commi

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,11 @@
 namespace API.Entities
 {
   public class AppUser
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }    
+  {
+    public int Id { get; set; }
+    public string UserName { get; set; }
 
-    }
+  }
 }
+
+// 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 
